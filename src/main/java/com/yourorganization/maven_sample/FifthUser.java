af6477fd7881c6a94400fa567fccb5aaa286bd5c
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-08-26 11:34:48 +0530

    Test

--- src/main/java/com/yourorganization/maven_sample/FifthUser.java
+++ src/main/java/com/yourorganization/maven_sample/FifthUser.java
@@ -22,9 +22,9 @@ public class FifthUser
 	  	   System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
 	  	   options.addArguments("--headless");
 	  	   options.addArguments("--window-size=1920,1200");
-	  	   options.addArguments("--ignore-certificate-errors");
-	  	// options.addArguments("-Djava.awt.headless=true");
-	  	 //options.addArguments("--allow-file-access-from-files");	
+	  	   options.addArguments("--disable-extensions");
+	  	   options.addArguments("-Djava.awt.headless=true");
+	  	   options.addArguments("--allow-file-access-from-files");	
 	       options.addArguments("--use-fake-device-for-media-stream");						
 	  	 //options.addArguments("--use-file-for-fake-video-capture=");
 	  	   options.addArguments("--use-file-for-fake-audio-capture=audio.wav");
