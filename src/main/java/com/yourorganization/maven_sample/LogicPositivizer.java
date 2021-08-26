83fc6ae372fb1c009a138bda915ea6cf26bdc4c0
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-08-26 09:57:58 +0530

    Test

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -12,39 +12,38 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class LogicPositivizer 
-{    WebDriver driver;
-    @Test
-    public void openapp()throws InterruptedException, AWTException 
-    {  
-
+{   WebDriver driver;
+@Test
+public void openapp()throws InterruptedException, AWTException 
+{  
      ChromeOptions options = new ChromeOptions();
 	 options.addArguments("--use-fake-ui-for-media-stream");
 	 System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
 	 options.addArguments("--headless");
+	 //options.addArguments("-Djava.awt.headless=true");
 	// options.addArguments("--allow-file-access-from-files");	
-     options.addArguments("--use-fake-device-for-media-stream");		
+     options.addArguments("--use-fake-device-for-media-stream");						
 	 //options.addArguments("--use-file-for-fake-video-capture=");
 	 options.addArguments("--use-file-for-fake-audio-capture=audio.wav");
 	 options.addArguments("--autoplay-policy=no-user-gesture-required");
 	 options.addArguments("--disable-infobars");	
 	 driver = new ChromeDriver(options);		
      driver.get("https://live.monetanalytics.com/stu_proc/student.html#"); 	
-     driver.manage().window().maximize();	
+     driver.manage().window().maximize();
 	 driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-	 Thread.sleep(2000);
      driver.findElement(By.xpath("//input[@class='bootbox-input bootbox-input-text form-control']")).sendKeys("AW-1");
-     System.out.println(driver.getTitle());
+     System.out.println(driver.getTitle());	
      Thread.sleep(2000);
      driver.findElement(By.xpath("//button[contains(text(),'OK')]")).click();	
      Robot rb=new Robot();		
-   	 Thread.sleep(8000);			
+   		
+     Thread.sleep(8000);			
      rb.keyPress(KeyEvent.VK_TAB);						
      rb.keyRelease(KeyEvent.VK_TAB);
+  
      Thread.sleep(1000);
      rb.keyPress(KeyEvent.VK_ENTER);
-     rb.keyRelease(KeyEvent.VK_ENTER);		
-     
-     
+     rb.keyRelease(KeyEvent.VK_ENTER);
      Thread.sleep(2000);
      rb.keyPress(KeyEvent.VK_TAB);			
   
@@ -56,7 +55,6 @@ public class LogicPositivizer
      rb.keyPress(KeyEvent.VK_ENTER);	
      rb.keyRelease(KeyEvent.VK_ENTER);       
     
-	 
     
     }
     
