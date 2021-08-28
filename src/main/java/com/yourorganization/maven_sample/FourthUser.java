8467b1a82bd0234b1ec6ab9ebdc52c8fb36799e6
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-08-28 16:04:23 +0530

    test

--- src/main/java/com/yourorganization/maven_sample/FourthUser.java
+++ src/main/java/com/yourorganization/maven_sample/FourthUser.java
@@ -21,9 +21,8 @@ public class FourthUser
 			 System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
 			 options.addArguments("--headless");
 			 options.addArguments("--window-size=1920,1200");
-			 
-			 options.addArguments("enable-automation");
-			
+		  	 //options.addArguments("--disable-extensions");
+			 options.addArguments("-Djava.awt.headless=true");
 			 options.addArguments("--allow-file-access-from-files");	
 		     options.addArguments("--use-fake-device-for-media-stream");						
 			 //options.addArguments("--use-file-for-fake-video-capture=");
