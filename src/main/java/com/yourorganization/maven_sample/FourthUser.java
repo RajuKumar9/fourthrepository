56244ca2ae358aa8c5fc5f104d25809988dfbd70
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-08-31 11:20:14 +0530

    Todaycommit

--- src/main/java/com/yourorganization/maven_sample/FourthUser.java
+++ src/main/java/com/yourorganization/maven_sample/FourthUser.java
@@ -22,7 +22,7 @@ public class FourthUser
 			 options.addArguments("--headless");
 			 options.addArguments("--window-size=1920,1200");
 		  	 //options.addArguments("--disable-extensions");
-			 options.addArguments("-Djava.awt.headless=true");
+			 //options.addArguments("-Djava.awt.headless=true");
 			 options.addArguments("--allow-file-access-from-files");	
 		     options.addArguments("--use-fake-device-for-media-stream");						
 			 //options.addArguments("--use-file-for-fake-video-capture=");
--- src/main/java/com/yourorganization/maven_sample/SecondUser.java
+++ src/main/java/com/yourorganization/maven_sample/SecondUser.java
@@ -24,7 +24,7 @@ public class SecondUser
 			 options.addArguments("--headless");
 			 options.addArguments("--window-size=1920,1200");
 		  	 //options.addArguments("--disable-extensions");
-			 options.addArguments("-Djava.awt.headless=false");
+			 //options.addArguments("-Djava.awt.headless=false");
 			 options.addArguments("--allow-file-access-from-files");	
 		     options.addArguments("--use-fake-device-for-media-stream");						
 			 //options.addArguments("--use-file-for-fake-video-capture=");
