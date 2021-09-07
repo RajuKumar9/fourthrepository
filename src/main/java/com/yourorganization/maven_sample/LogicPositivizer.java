38a9b0f869357d5ccf77c64fc7e45c0785e58ffe
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-09-07 11:31:18 +0530

    changes

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -34,7 +34,7 @@ public void openapp()
      driver.get("https://live.monetanalytics.com/stu_proc/student.html#"); 	
      driver.manage().window().maximize();
 	 driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-     driver.findElement(By.xpath("//input[@class='bootbox-input bootbox-input-text form-control']")).sendKeys("Student-1");
+     driver.findElement(By.xpath("//input[@class='bootbox-input bootbox-input-text form-control']")).sendKeys("Demo-1");
      System.out.println(driver.getTitle());	
      Thread.sleep(2000);
      driver.findElement(By.xpath("//button[contains(text(),'OK')]")).click();	
--- src/main/java/com/yourorganization/maven_sample/SecondUser.java
+++ src/main/java/com/yourorganization/maven_sample/SecondUser.java
@@ -35,7 +35,7 @@ public class SecondUser
 		     driver.get("https://live.monetanalytics.com/stu_proc/student.html#"); 	
 		     driver.manage().window().maximize();
 			 driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-		     driver.findElement(By.xpath("//input[@class='bootbox-input bootbox-input-text form-control']")).sendKeys("Student-2");
+		     driver.findElement(By.xpath("//input[@class='bootbox-input bootbox-input-text form-control']")).sendKeys("Demo-2");
 		     System.out.println(driver.getTitle());	
 		     Thread.sleep(2000);
 		     driver.findElement(By.xpath("//button[contains(text(),'OK')]")).click();	
