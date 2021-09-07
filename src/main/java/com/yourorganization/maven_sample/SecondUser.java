7ae99e96e4e63bd9140439b8d00bbddcf57bbb42
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-09-07 12:08:08 +0530

    test

--- src/main/java/com/yourorganization/maven_sample/SecondUser.java
+++ src/main/java/com/yourorganization/maven_sample/SecondUser.java
@@ -35,7 +35,7 @@ public class SecondUser
 		     driver.get("https://live.monetanalytics.com/stu_proc/student.html#"); 	
 		     driver.manage().window().maximize();
 			 driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-		     driver.findElement(By.xpath("//input[@class='bootbox-input bootbox-input-text form-control']")).sendKeys("Demo-2");
+		     driver.findElement(By.xpath("//input[@class='bootbox-input bootbox-input-text form-control']")).sendKeys("Student-2");
 		     System.out.println(driver.getTitle());	
 		     Thread.sleep(2000);
 		     driver.findElement(By.xpath("//button[contains(text(),'OK')]")).click();	
@@ -45,7 +45,6 @@ public class SecondUser
 		     rb.keyPress(KeyEvent.VK_TAB);						
 		     rb.keyRelease(KeyEvent.VK_TAB);
 		  
-		     Thread.sleep(1000);
 		     rb.keyPress(KeyEvent.VK_ENTER);
 		     rb.keyRelease(KeyEvent.VK_ENTER);
 		     Thread.sleep(2000);
