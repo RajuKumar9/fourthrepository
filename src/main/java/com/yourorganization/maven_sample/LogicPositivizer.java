e3f84af1f57a592bf00b4cc5d9ca412952b48e0f
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-09-07 10:02:18 +0530

    changes in Logical and second class

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -19,7 +19,7 @@ public void openapp()
      {  
      ChromeOptions options = new ChromeOptions();
 	 options.addArguments("--use-fake-ui-for-media-stream");
-	 System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+	 System.setProperty("webdriver.chrome.driver","//usr/bin/chromedriver");
 	 options.addArguments("--headless");
 	 options.addArguments("--window-size=1280,700");
 	 //options.addArguments("--disable-extensions");
