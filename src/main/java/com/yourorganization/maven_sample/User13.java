05d024efb101674872517c92baa0c581b5bd8526
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-09-15 17:08:08 +0530

    changes

--- src/main/java/com/yourorganization/maven_sample/User13.java
+++ src/main/java/com/yourorganization/maven_sample/User13.java
@@ -59,7 +59,7 @@ public void openapp()
      Thread.sleep(2000);
      rb.keyPress(KeyEvent.VK_ENTER);	
      rb.keyRelease(KeyEvent.VK_ENTER);   
-     Thread.sleep(1800000);
+     Thread.sleep(300000);
      driver.close();;
      }catch(Throwable e)
      {
