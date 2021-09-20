fd486b3cae2042838150af2ddbddbec8e2893666
RajuKumar9 <88493241+RajuKumar9@users.noreply.github.com>
2021-09-20 16:26:37 +0530

    changes

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -57,7 +57,7 @@ public void openapp()
      Thread.sleep(2000);
      rb.keyPress(KeyEvent.VK_ENTER);	
      rb.keyRelease(KeyEvent.VK_ENTER); 
-     //Thread.sleep(300000);
+     Thread.sleep(300000);
      driver.close();
       }catch(Throwable e)
       { 
